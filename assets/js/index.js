11eb7d5681bbb5f51898630b6184de07ea1b8a50
Todd Lee <toddtlee48@gmail.com>
2020-09-27 21:37:34 -0400

    merging

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+$("#q1").click(function() {
+	$("#a1").fadeToggle("slow");
+	$("#arrow1-down").toggleClass("collapse");
+	$("#arrow1-up").toggleClass("collapse");
+})
+
+$("#q2").click(function() {
+	$("#a2").fadeToggle("slow");
+	$("#arrow2-down").toggleClass("collapse");
+	$("#arrow2-up").toggleClass("collapse");
+})
+
+$("#q3").click(function() {
+	$("#a3").fadeToggle("slow");
+	$("#arrow3-down").toggleClass("collapse");
+	$("#arrow3-up").toggleClass("collapse");
+})
+
+$("#q4").click(function() {
+	$("#a4").fadeToggle("slow");
+	$("#arrow4-down").toggleClass("collapse");
+	$("#arrow4-up").toggleClass("collapse");
+})
+
